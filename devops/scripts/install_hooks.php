dcfeb1440909884909e356828e15b5647cb51d61
Elbek Azimov <atom.azimov@gmail.com>
2016-03-30 13:00:35 +0500

    Dockerize

--- devops/scripts/install_hooks.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * Copyright © 2016 Elbek Azimov. Contacts: <atom.azimov@gmail.com>.
- */
-$root = realpath(__DIR__.'/../../');
-
-@copy($root.'/devops/scripts/hooks/pre-commit', $root.'/.git/hooks/pre-commit');
